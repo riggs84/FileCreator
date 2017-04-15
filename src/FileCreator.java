c6e897c54b395ea35b7f479d495b8582b11e288b
riggs84 <victor.iurkov@gmail.com>
2017-04-15 10:52:59 -0400

    new fetures added

--- src/FileCreator.java
+++ src/FileCreator.java
@@ -23,6 +23,7 @@ public class FileCreator {
         mainFrame = new JFrame("File creator tool");
         mainFrame.setSize(600,150);
         mainFrame.setLayout(new GridLayout(3,1));
+        mainFrame.getRootPane().setBorder(BorderFactory.createMatteBorder(4,4,4,4, Color.LIGHT_GRAY));
         mainFrame.setLocationRelativeTo(null);
         mainFrame.addWindowListener(new WindowListener() {
             @Override
@@ -63,6 +64,7 @@ public class FileCreator {
         });
         controlPanel = new JPanel();
         controlPanel.setLayout(new BoxLayout(controlPanel,BoxLayout.X_AXIS));
+        controlPanel.setBorder(new EmptyBorder(0,10,0,10));
         JLabel header = new JLabel("Welcome to file creation tool.",SwingConstants.CENTER);
         //JLabel footer = new JLabel("",SwingConstants.CENTER);
         footer = new JPanel(new FlowLayout());
@@ -74,7 +76,9 @@ public class FileCreator {
     private void start()
     {
         JLabel file = new JLabel("Full path to file:");
+        file.setBorder(new EmptyBorder(0,0,0,5));
         JLabel size = new JLabel("file size:");
+        size.setBorder(new EmptyBorder(0,5,0,5));
         JTextField fPath = new JTextField();
         JTextField fSize = new JTextField();
         JButton createBtn = new JButton("Create");
@@ -114,6 +118,7 @@ public class FileCreator {
         fDimension.addElement("Bytes");*/
         String[] fDimension = {"MBytes", "KBytes", "Bytes"};
         final JComboBox fileSizeDim = new JComboBox(fDimension);
+        fileSizeDim.setBorder(new EmptyBorder(0,5,0,5));
         fileSizeDim.setSelectedIndex(2);
         fileSizeDim.addActionListener(new ActionListener() {
             @Override
